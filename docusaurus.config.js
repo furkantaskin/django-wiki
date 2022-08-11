6ef239e03577874602f95c335f8daf77640a3706
Furkan Taşkın <furkan.taskin@tuta.io>
2022-08-11 14:11:30 +0300

    Edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'giris',
+            docId: 'intro',
             position: 'left',
             label: 'Notlar',
           },
